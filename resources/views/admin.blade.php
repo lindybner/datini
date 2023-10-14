159f76797c5e8a7d082713c23bf7762d5016f76b
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 15:46:03 -0400

    Update admin.blade.php
    basic admin dashboard setup

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin</title>
+    <title>Admin Dashboard</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/app.css">
     <script src="/app.js"></script>
 </head>
 <body>
+
+    <header class="m-5">
+        <h1 class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">Admin Dashboard</h1>
+    </header>
+
+    <section class="p-3 m-5 border border-primary-subtle rounded-3">
+        <h2 class="text-primary-emphasis">Manage Users</h2>
+    </section>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
