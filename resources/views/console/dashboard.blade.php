a025d660a47dcf4a1c2924c8e6dad7f45f945bac
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 16:39:08 -0400

    Update dashboard.blade.php
    copy adjustment

--- resources/views/console/dashboard.blade.php
+++ resources/views/console/dashboard.blade.php
@@ -28,7 +28,8 @@
     </header>
 
     <section class="p-3 m-5 border border-primary-subtle rounded-3">
-        <h2 class="text-primary-emphasis">Manage Users</h2>
+        <h2 class="text-primary-emphasis">Three simple steps to better financial health!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit veniam ut eius provident perferendis molestiae neque suscipit voluptatum consequuntur quas quam vel natus, in, placeat dolores deleniti impedit tempore accusantium?</p>
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
