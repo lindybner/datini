27e78296946cb82e401dd01ba2898bf5affcb82e
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 12:51:38 -0400

    Create 2023_10_14_165103_create_flows_table.php
    cash flows table created in db

---
+++ database/migrations/2023_10_14_165103_create_flows_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flows', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('month_id');
+            $table->foreignId('user_id');
+            $table->decimal('inflow', 10, 2);
+            $table->decimal('outflow', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flows');
+    }
+};
