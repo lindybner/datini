6d0595ea01612d72b906ff70c1e638f106843d7f
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 15:55:32 -0400

    Update AdminController.php
    admin login & logout methods added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,10 +6,39 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
-
+    // Return Admin Dashboard view
     public function admin()
     {
         return view('admin');
+    }
+
+    // Return Admin Form view
+    public function loginForm()
+    {
+        return view('admin.login');
+    }
+
+    // Process login form on submission
+    public function login()
+    {
+        $attributes = request()->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            return redirect('/admin');
+        }
+
+        return back()
+            ->withInput()
+            ->withErrors(['username' => 'Invalid login credentials']);
+    }
+
+    // Logout
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/');
     }
 }
