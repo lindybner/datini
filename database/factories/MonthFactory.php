57fb25cf7a9565a30098dedc399c2a742548642c
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 13:07:15 -0400

    Create MonthFactory.php

---
+++ database/factories/MonthFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Month>
+ */
+class MonthFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'month' => $this->faker->monthName, // Use faker to generate a random month name
+            'year' => $this->faker->year, // Use faker to generate a random year
+        ];
+    }
+}
