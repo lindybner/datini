e94c786ace8149741bb7f4d0d2044e53b4df68fd
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 12:50:52 -0400

    Create 2023_10_14_164757_create_balances_table.php
    balance sheet table created in db

---
+++ database/migrations/2023_10_14_164757_create_balances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('balances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('month_id');
+            $table->foreignId('user_id');
+            $table->decimal('asset', 10, 2);
+            $table->decimal('liability', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('balances');
+    }
+};
