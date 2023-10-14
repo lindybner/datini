b4815e4a02bfb9b30e3f880398640bffa6b360e9
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 16:23:25 -0400

    Update web.php
    middleware added for authentication to dashboard, login & logout routes

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ Route::get('/', function () {
 });
 
 // Admin Dashboard
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
+Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->middleware('auth');
 
 // Admin Login - Display Form
-Route::get('/admin/login', [AdminController::class, 'loginForm']);
+Route::get('/admin/login', [AdminController::class, 'loginForm'])->middleware('guest');
 // Admin Login - Process Form
-Route::post('/admin/login', [AdminController::class, 'login']);
+Route::post('/admin/login', [AdminController::class, 'login'])->middleware('guest');
 // Admin Logout
-Route::get('/admin/logout', [AdminController::class, 'logout']);
+Route::get('/admin/logout', [AdminController::class, 'logout'])->middleware('auth');
