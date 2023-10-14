e3704864e0d3d8087644d9438ebbf31114e62bb2
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-14 17:47:54 -0400

    Update overview.blade.php
    minor navbar adjustments

--- resources/views/console/overview.blade.php
+++ resources/views/console/overview.blade.php
@@ -15,12 +15,15 @@
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">Datini</a>
+                <a class="navbar-brand fw-semibold" href="/">Datini</a>
+                <span class="navbar-text fst-italic">
+                    Personal finances in 3 simple steps!
+                </span>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                     <a class="nav-link active" href="/console/overview">Overview</a>
                     </li>
