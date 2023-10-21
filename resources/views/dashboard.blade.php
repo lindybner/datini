f12d428690960ad339a644fb958f6e3a6205d0b7
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-21 16:30:16 -0400

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -57,10 +57,34 @@
         </div>
     </header>
 
-    <section class="p-3 m-5 border border-primary-subtle rounded-3">
-        <h2 class="text-primary-emphasis">Three simple steps to better financial health!</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit veniam ut eius provident perferendis molestiae neque suscipit voluptatum consequuntur quas quam vel natus, in, placeat dolores deleniti impedit tempore accusantium?</p>
-    </section>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Overview &amp; Months</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title</h2>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h2 class="card-title">Card title</h2>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
