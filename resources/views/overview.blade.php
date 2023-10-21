e944e8cb389a1849da3aff83a978c0f06100a0bc
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-21 17:01:43 -0400

    Create overview.blade.php

---
+++ resources/views/overview.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Overview</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-semibold" href="/">Datini</a>
+                <span class="navbar-text fst-italic">
+                    In 3 simple steps, get all your personal finances in order!
+                </span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/overview">Overview</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/flow">Flow</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/balance">Balance</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 m-5">
+            <h1>
+                Overview
+            </h1>
+            <?php if(Auth::check()): ?>
+                Logged in as
+                <strong><?= auth()->user()->username ?></strong>!
+
+            <?php else: ?>
+                <a href="/">Return to Homepage</a>
+            <?php endif; ?>
+        </div>
+    </header>
+
+    <div class="container">
+        <h2>Months</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Month</th>
+                    <th scope="col">Year</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <?php foreach($months as $key => $value): ?>
+                <tr>
+                    <td><?= $value->$id ?></td>
+                    <td><?= $value->$month ?></td>
+                    <td><?= $value->$year ?></td>
+                    <td><a href="/overview/edit/<?= $value->id ?>">Edit</a></td>
+                    <td><a href="/overview/delete/<?= $value->id ?>">Delete</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+        <a href="/overview/add">Add a new month</a>
+    </div>
+
+</body>
+</html>
