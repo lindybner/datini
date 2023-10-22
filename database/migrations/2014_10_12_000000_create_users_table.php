23f0b3cc0047f4881c6b9c77b9fd20ee65623eff
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-22 14:38:47 -0400

    Revert "Update 2014_10_12_000000_create_users_table.php"
    This reverts commit 576d43a727589f9d01ca74831720a117f018a91a.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
+            // $table->rememberToken();
             // $table->timestamps();
         });
     }
