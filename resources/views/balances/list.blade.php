aac7ae99297cc207483ac22431d332696d592ab0
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-22 13:20:15 -0400

    Update list.blade.php

--- resources/views/balances/list.blade.php
+++ resources/views/balances/list.blade.php
@@ -81,7 +81,7 @@
                     <td><?= $value->month->month ?> <?= $value->month->year ?></td>
                     <td><?= number_format($value->asset, 2, '.', ',') ?></td>
                     <td><?= number_format($value->liability, 2, '.', ',') ?></td>
-                    <td><?= number_format($value->liability / $value->asset * 100, 2, '.', ',') ?>%</td>
+                    <td><?= number_format($value->liability / $value->asset * 100, 2, '.', ',') ?></td>
                     <td><?= number_format($value->asset - $value->liability, 2, '.', ',') ?></td>
                     <td><a href="/balances/edit/<?= $value->id ?>">Edit</a> | <a class="text-danger" href="/balances/delete/<?= $value->id ?>">Delete</a></td>
                 </tr>
