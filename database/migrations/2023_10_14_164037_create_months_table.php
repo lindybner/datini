189ff54ca23263a23f39161d2269320f7c68ae56
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-22 14:39:04 -0400

    Revert "Update 2023_10_14_164037_create_months_table.php"
    This reverts commit e43de002a480cf3332ce651c14e907e85207e3aa.

--- database/migrations/2023_10_14_164037_create_months_table.php
+++ database/migrations/2023_10_14_164037_create_months_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('month');
             $table->string('year');
             // $table->foreignId('user_id');
-            // $table->timestamps();
+            $table->timestamps();
         });
     }
 
