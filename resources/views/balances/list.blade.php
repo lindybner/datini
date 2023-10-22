8d9ae0c4d8dddb6695c15bc758028395460e233f
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-22 12:58:40 -0400

    debt ratio implemented

--- resources/views/balances/list.blade.php
+++ resources/views/balances/list.blade.php
@@ -70,6 +70,7 @@
                     <th scope="col">Month</th>
                     <th scope="col">Asset</th>
                     <th scope="col">Liability</th>
+                    <th scope="col">Debt Ratio</th>
                     <th scope="col">Net Worth</th>
                     <th scope="col">Action</th>
                 </tr>
@@ -80,6 +81,7 @@
                     <td><?= $value->month->month ?> <?= $value->month->year ?></td>
                     <td>$<?= number_format($value->asset, 2, '.', ',') ?></td>
                     <td>$<?= number_format($value->liability, 2, '.', ',') ?></td>
+                    <td><?= number_format($value->liability / $value->asset * 100, 2, '.', ',') ?>%</td>
                     <td><strong>$<?= number_format($value->asset - $value->liability, 2, '.', ',') ?></strong></td>
                     <td><a href="/balances/edit/<?= $value->id ?>">Edit</a> | <a class="text-danger" href="/balances/delete/<?= $value->id ?>">Delete</a></td>
                 </tr>
