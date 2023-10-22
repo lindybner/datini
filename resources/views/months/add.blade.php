a9aa6c6ec64286b3af1e0f5976076f4e3271f162
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-10-22 13:50:04 -0400

    Create add.blade.php

---
+++ resources/views/months/add.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Overview</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-semibold" href="/">Datini</a>
+                <span class="navbar-text fst-italic">
+                    In 3 simple steps, get all your personal finances in order!
+                </span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/dashboard">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/months/list">Months</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/balances/list">Balances</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/flows/list">Flows</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 m-5">
+            <h1>
+                Add a new month
+            </h1>
+            <?php if(Auth::check()): ?>
+                Logged in as
+                <strong><?= auth()->user()->username ?></strong>!
+
+            <?php else: ?>
+                <a href="/">Return to Homepage</a>
+            <?php endif; ?>
+        </div>
+    </header>
+
+    <div class="container">
+        <form action="/months/add" method="POST" novalidate>
+            <?= csrf_field() ?>
+
+            <div>
+                <label for="month">Select Month:</label>
+                <select id="month" name="month">
+                    <option value="January">January</option>
+                    <option value="February">February</option>
+                    <option value="March">March</option>
+                    <option value="April">April</option>
+                    <option value="May">May</option>
+                    <option value="June">June</option>
+                    <option value="July">July</option>
+                    <option value="August">August</option>
+                    <option value="September">September</option>
+                    <option value="October">October</option>
+                    <option value="November">November</option>
+                    <option value="December">December</option>
+                </select>
+            </div>
+            <div>
+                <label for="year">Enter Year:</label>
+                <input type="number" name="year" id="year">
+            </div>
+            <button type="submit">Add Month</button>
+        </form>
+
+        <a href="/months/list">Back to Months</a>
+    </div>
+
+</body>
+</html>
