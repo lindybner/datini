7450f7d9a5193acaa9ed649071be0b7161eebf6c
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-01 16:36:36 -0400

    Update list.blade.php

--- resources/views/months/list.blade.php
+++ resources/views/months/list.blade.php
@@ -71,7 +71,7 @@
                     <th scope="col">Month</th>
                     <th scope="col">Year</th>
                     <th scope="col">Net Worth ($)</th>
-                    <th scope="col">Cash Flow ($)</th>
+                    <th scope="col">Savings ($)</th>
                 </tr>
             </thead>
             <tbody>
@@ -81,14 +81,19 @@
                     <td><?= $value->month ?></td>
                     <td><?= $value->year ?></td>
                     <!-- Display balance data if available -->
-                        <?php if($value->balance): ?>
-                            <td><?= number_format($value->balance->asset - $value->balance->liability, 2, '.', ',') ?></td>
-                            <td><?= $value->balance->liability ?></td>
-                        <?php else: ?>
-                            <!-- If no balance data is available, display placeholders or an empty cell -->
-                            <td>N/A</td>
-                            <td>N/A</td>
-                        <?php endif; ?>
+                    <?php if($value->balance): ?>
+                        <td><?= number_format($value->balance->asset - $value->balance->liability, 2, '.', ',') ?></td>
+                    <?php else: ?>
+                        <!-- If no balance data is available, display placeholders or an empty cell -->
+                        <td>N/A</td>
+                    <?php endif; ?>
+                    <!-- Display flow data if available -->
+                    <?php if($value->flow): ?>
+                        <td><?= number_format($value->flow->inflow - $value->flow->outflow, 2, '.', ',') ?></td>
+                    <?php else: ?>
+                        <!-- If no flow data is available, display placeholders or an empty cell -->
+                        <td>N/A</td>
+                    <?php endif; ?>
                 </tr>
             <?php endforeach; ?>
             </tbody>
