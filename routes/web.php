1f1fb774cd9557f8b4c504e814309295032b898d
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-02 14:58:03 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::get('/logout', [AppController::class, 'logout'])->middleware('auth');
 Route::get('/months/add', [MonthsController::class, 'addForm'])->middleware('auth');
 Route::post('/months/add', [MonthsController::class, 'add'])->middleware('auth');
 // Create - Balances
+Route::get('/balances/add', [MonthsController::class, 'addForm'])->middleware('auth');
+Route::post('/balances/add', [MonthsController::class, 'add'])->middleware('auth');
 
 // Create - Flows
 
