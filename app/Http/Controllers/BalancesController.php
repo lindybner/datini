9b3971a8a64ebdddc81f3d43fa704a69d997e158
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-02 15:02:05 -0400

    Balances/Add form file creation

--- app/Http/Controllers/BalancesController.php
+++ app/Http/Controllers/BalancesController.php
@@ -23,4 +23,25 @@ class BalancesController extends Controller
         return redirect('/balances/list')
             ->with('message', 'Balance record deleted');
     }
+
+    public function addForm()
+    {
+        return view('balances.add');
+    }
+
+    public function add()
+    {
+        $attributes = request()->validate([
+            'month' => 'required',
+            'year' => 'required',
+        ]);
+
+        $month = new Month();
+        $month->month = $attributes['month'];
+        $month->year = $attributes['year'];
+        $month->save();
+
+        return redirect('/months/list')
+            ->with('message', 'New month added.');
+    }
 }
