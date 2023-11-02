51716da03d4bad32184e95cf31c436ebb95acc8d
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-02 16:43:54 -0400

    Update FlowsController.php

--- app/Http/Controllers/FlowsController.php
+++ app/Http/Controllers/FlowsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Flow;
+use App\Models\Month;
 use Illuminate\Http\Request;
 
 class FlowsController extends Controller
@@ -22,5 +23,29 @@ class FlowsController extends Controller
 
         return redirect('/flows/list')
             ->with('message', 'Cash flow record deleted');
+    }
+
+    public function addForm()
+    {
+        $months = Month::all();
+        return view('flows.add', compact('months'));
+    }
+
+    public function add()
+    {
+        $attributes = request()->validate([
+            'month_id' => 'required',
+            'inflow' => 'required',
+            'outflow' => 'required',
+        ]);
+
+        $balance = new Flow();
+        $balance->month_id = $attributes['month_id'];
+        $balance->inflow = $attributes['inflow'];
+        $balance->outflow = $attributes['outflow'];
+        $balance->save();
+
+        return redirect('/flows/list')
+            ->with('message', 'New cash flow added.');
     }
 }
