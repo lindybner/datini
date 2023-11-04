3e942d801d1564f3b45e04f692827020084a5bec
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 13:06:18 -0400

    Update edit.blade.php

--- resources/views/balances/edit.blade.php
+++ resources/views/balances/edit.blade.php
@@ -90,7 +90,7 @@
                 <br>
             <?php endif; ?>
 
-            <button type="submit" class="btn btn-primary my-3">Add Balance</button>
+            <button type="submit" class="btn btn-primary my-3">Edit Balance</button>
         </form>
 
 
