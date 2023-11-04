9d4432f90062681b22d2ee87210bd667f321bbcc
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 14:08:55 -0400

    Update Balance.php

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -9,6 +9,18 @@ class Balance extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string, mixed>
+     */
+    protected $fillable = [
+        'month_id',
+        'asset',
+        'liability',
+        'user_id', // Assuming you have a user_id field in your balances table
+    ];
+
     public function month()
     {
         return $this->belongsTo(Month::class, 'month_id');
