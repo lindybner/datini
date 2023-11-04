2bb1d2e89051c3fce222300374100d8b485282db
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 17:05:44 -0400

    blade extend layout applied for header to dashboard and login views

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Datini</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-semibold" href="/">Datini</a>
+                <span class="navbar-text fst-italic">
+                    In 3 simple steps, get all your personal finances in order!
+                </span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/dashboard">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/months/list">Months</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/balances/list">Balances</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/flows/list">Flows</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+    <!-- Content Section -->
+    <div class="content">
+        @yield('content')
+    </div>
+
+    <!-- Common Footer -->
+    <footer>
+        <!-- Your footer content goes here -->
+    </footer>
+</body>
+</html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,19 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+@extends('layouts.layout')
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
+@section('content')
+        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 m-5">
+            <h1>
+                Login
+            </h1>
+            <?php if(Auth::check()): ?>
+                Logged in as
+                <strong><?= auth()->user()->username ?></strong>!
 
-    <header class="m-5">
-        <h1 class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">Login</h1>
+            <?php else: ?>
+                <a href="/">Return to Homepage</a>
+            <?php endif; ?>
+        </div>
     </header>
 
     <section class="p-3 m-5 border border-primary-subtle rounded-3">
@@ -50,3 +49,4 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
+@endsection
