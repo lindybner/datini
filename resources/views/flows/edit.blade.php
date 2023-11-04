09b744b7a7f83d20d755f7f39454d1559bb3fad8
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 13:04:47 -0400

    Create edit.blade.php

---
+++ resources/views/flows/edit.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Overview</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+</head>
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-semibold" href="/">Datini</a>
+                <span class="navbar-text fst-italic">
+                    In 3 simple steps, get all your personal finances in order!
+                </span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/dashboard">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/months/list">Months</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/balances/list">Balances</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/flows/list">Flows</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 m-5">
+            <h1>
+                Edit a cash flow
+            </h1>
+            <?php if(Auth::check()): ?>
+                Logged in as
+                <strong><?= auth()->user()->username ?></strong>!
+
+            <?php else: ?>
+                <a href="/">Return to Homepage</a>
+            <?php endif; ?>
+        </div>
+    </header>
+
+    <div class="container">
+        <form action="/flows/edit/<?= $flow->id ?>" method="POST" novalidate>
+            <?= csrf_field() ?>
+
+            <div class="my-3">
+                <label for="month_id">Month:</label>
+                <select name="month_id" id="month_id" class="form-select">
+                    @foreach($months as $month)
+                        <option value="{{ $month->id }}">{{ $month->month }} {{ $month->year }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="my-3">
+                <label for="inflow">Inflow:</label>
+                <input type="number" name="inflow" id="inflow" value="<?= old('inflow', $flow->inflow) ?>">
+            </div>
+
+            <?php if($errors->first('inflow')): ?>
+                <span class="text-danger"><?= $errors->first('inflow') ?></span>
+                <br>
+            <?php endif; ?>
+            
+            <div class="my-3">
+                <label for="outflow">Outflow:</label>
+                <input type="number" name="outflow" id="outflow" value="<?= old('outflow', $flow->outflow) ?>">
+            </div>
+
+            <?php if($errors->first('outflow')): ?>
+                <span class="text-danger"><?= $errors->first('outflow') ?></span>
+                <br>
+            <?php endif; ?>
+
+            <button type="submit" class="btn btn-primary my-3">Add Balance</button>
+        </form>
+
+
+        <a href="/flows/list">Back to Cash Flows</a>
+    </div>
+
+</body>
+</html>
