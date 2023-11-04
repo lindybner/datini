8c7adae6058cb7e2388bd1600191fa1564949616
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 13:38:56 -0400

    Update BalanceFactory.php

--- database/factories/BalanceFactory.php
+++ database/factories/BalanceFactory.php
@@ -21,7 +21,7 @@ class BalanceFactory extends Factory
         return [
             'asset' => $this->faker->randomFloat(2, 1000, 100000), // Generate a random asset amount with 2 decimal places
             'liability' => $this->faker->randomFloat(2, 500, 50000), // Generate a random liability amount with 2 decimal places
-            // 'user_id' => User::all()->random(), // Grab a random id from User
+            'user_id' => User::all()->random(), // Grab a random id from User
             'month_id' => Month::all()->random(), // Grab a random id from Month
         ];
     }
