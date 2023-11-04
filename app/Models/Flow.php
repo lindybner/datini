bb622a5dc1ce628f6d313eda5c6fbbbba60b8979
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 14:12:42 -0400

    Update Flow.php

--- app/Models/Flow.php
+++ app/Models/Flow.php
@@ -9,6 +9,18 @@ class Flow extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string, mixed>
+     */
+    protected $fillable = [
+        'month_id',
+        'inflow',
+        'outflow',
+        'user_id', // Assuming you have a user_id field in your flows table
+    ];
+
     public function month()
     {
         return $this->belongsTo(Month::class, 'month_id');
