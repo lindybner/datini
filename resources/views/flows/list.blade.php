e86fc4e4477c707333fa315b4c584a4229bbb79a
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 17:13:49 -0400

    blade layout extend applied to flows views

--- resources/views/flows/list.blade.php
+++ resources/views/flows/list.blade.php
@@ -1,48 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cash Flows</title>
+@extends('layouts.layout')
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
+@section('content')
 
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary px-5 py-3">
-            <div class="container-fluid">
-                <a class="navbar-brand fw-semibold" href="/">Datini</a>
-                <span class="navbar-text fst-italic">
-                    In 3 simple steps, get all your personal finances in order!
-                </span>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/dashboard">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/months/list">Months</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/balances/list">Balances</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/flows/list">Flows</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/logout">Logout</a>
-                    </li>
-                </ul>
-                </div>
-            </div>
-        </nav>
         <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 m-5">
             <h1>
                 Cash Flows
@@ -93,3 +52,4 @@
 
 </body>
 </html>
+@endsection
