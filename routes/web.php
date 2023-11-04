5c8fe5e733a0d8080a4d2cead2dbb963159c4b48
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-04 15:51:17 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 });
 
 // Dashboard
+Route::get('/', [AppController::class, 'dashboard'])->middleware('auth');
 Route::get('/dashboard', [AppController::class, 'dashboard'])->middleware('auth');
 
 // Login - Display Form
