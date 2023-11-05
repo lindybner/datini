9682d0c00a96f6803e4003cf32bcd5cf8d535c88
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-05 10:42:17 -0500

    Update list.blade.php

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -30,6 +30,7 @@
                     <th scope="col">Action</th>
                     <th scope="col">ID</th>
                     <th scope="col">Username</th>
+                    <th scope="col">Password</th>
                     <th scope="col">Admin</th>
                 </tr>
             </thead>
@@ -40,6 +41,7 @@
                     <td><a href="/users/edit/<?= $value->id ?>">Edit</a> | <a class="text-danger" href="/users/delete/<?= $value->id ?>">Delete</a></td>
                     <td><?= $value->id ?></td>
                     <td><?= $value->username ?></td>
+                    <td><?= $value->password ?></td>
                     <td><?= $value->is_admin ?></td>
                 </tr>
 
