1cd576e24ba0d398781410b473037f20f7df9bfa
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-05 15:05:28 -0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,8 +23,11 @@
             <div class="col">
                 <div class="card h-100">
                     <div class="card-body">
-                        <h2 class="card-title">Step 1</h2>
-                        <p class="card-text"><strong>Set the Time Period:</strong> Enter the "month" and "year" for the period you are entering statement balances for. If it's already in the database, go to step 2!</p>
+                        <h2 class="card-title">
+                            <div>Step 1:</div>
+                            <div>Set the Time Period</div>
+                        </h2>
+                        <p class="card-text">Enter the "month" and "year" for the period you are entering statement balances for. If it's already in the database, go to step 2!</p>
                         <a class="btn btn-primary" href="/months/list" role="button">Months</a>
                     </div>
                 </div>
@@ -32,8 +35,11 @@
             <div class="col">
                 <div class="card h-100">
                     <div class="card-body">
-                        <h2 class="card-title">Step 2</h2>
-                        <p class="card-text"><strong>Asset &amp; Liability:</strong> Once you've selected your period, enter the balances on your statements for assets (what you own) and liabilities (what you owe).</p>
+                        <h2 class="card-title">
+                            <div>Step 2:</div>
+                            <div>Asset &amp; Liability</div>
+                        </h2>
+                        <p class="card-text">Once you've selected your period, enter the balances on your statements for assets (what you own) and liabilities (what you owe).</p>
                         <a class="btn btn-primary" href="/balances/list" role="button">Balances</a>
                     </div>
                 </div>
@@ -41,8 +47,11 @@
             <div class="col">
                 <div class="card h-100">
                     <div class="card-body">
-                        <h2 class="card-title">Step 3</h2>
-                        <p class="card-text"><strong>Cash Flow:</strong> Finally, figure out your cashflow &mdash; enter the amount for your inflow (what you've earned) and outflow (what you've spent).</p>
+                        <h2 class="card-title">
+                            <div>Step 3:</div>
+                            <div>Cash inflow &amp; outflow</div>
+                        </h2>
+                        <p class="card-text">Finally, figure out your cashflow &mdash; enter the amount for your inflow (what you've earned) and outflow (what you've spent).</p>
                         <a class="btn btn-primary" href="/flows/list" role="button">Flows</a>
                     </div>
                 </div>
