81ef3e032e9b85bb2d7d1e403849d982906105ee
Jim Lin-Dybner <lin.jycl@gmail.com>
2023-11-05 09:24:10 -0500

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     //
+
+    public function list()
+    {
+        return view('users.list', [
+            'users' => User::all()
+        ]);
+    }
 }
